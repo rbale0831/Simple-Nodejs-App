ad1a2f5b167cb35697b4e00016a2c51e74d0b1d8
rbale3108 <rbale0831@eng.rizvi.edu.in>
2023-11-21 17:45:08 +0530

    edit in app.js file

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req,res){
     res.send('Hello World from AWS Fargate!');
 });
 
-app.listen(80, function (){
-    console.log('App listening on port 80!');
+app.listen(4957, function (){
+    console.log('App listening on port 4957!');
 });
