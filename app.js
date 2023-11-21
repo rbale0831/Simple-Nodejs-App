1b5596beafbd6eeb766c7a1c27132dc1003caab4
rbale3108 <rbale0831@eng.rizvi.edu.in>
2023-11-21 17:35:36 +0530

    upload all file on git

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+app.get('/', function (req,res){
+    res.send('Hello World from AWS Fargate!');
+});
+
+app.listen(80, function (){
+    console.log('App listening on port 80!');
+});
